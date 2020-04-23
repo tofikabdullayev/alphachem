bbb2cd03ded3d3a678519ead6c69ebc58e3a5cf9
Tofik Abdullayev <tofiq.a@goldenpay.az>
2020-04-23 22:26:44 +0200

    added material ui and redux

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return <div className="App">Admin panel is loading ...</div>;
+  return (
+    <div className="App">
+      Admin panel is loading ...{' '}
+      <Button variant="contained" color="primary">
+        Start
+      </Button>
+    </div>
+  );
 }
 
 export default App;
