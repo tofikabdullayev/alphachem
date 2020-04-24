04316722f91169b9b559dbd98862a5e2ce2709e1
Tofik Abdullayev <tofiq.a@goldenpay.az>
2020-04-24 22:46:54 +0200

    minor improvement

--- admin/src/components/Layout/styles.ts
+++ admin/src/components/Layout/styles.ts
@@ -12,7 +12,7 @@ export default makeStyles((theme: Theme) =>
       width: '300px',
     },
     contentWrapper: {
-      width: 'calc(100% - 300px)',
+      width: '100%',
       height: '100%',
     },
   })
