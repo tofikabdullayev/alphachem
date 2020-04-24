5f990bab02b848125b357bc8debb0a06b0d88f42
Tofig Abdullayev <tofig.abdullayev@motorolasolutions.com>
2020-04-24 17:33:33 +0200

    minor change in layout

--- admin/src/components/Layout/index.tsx
+++ admin/src/components/Layout/index.tsx
@@ -10,12 +10,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <div>header</div>
       <Grid container spacing={3}>
         <Grid item sm={3}>
           <div>Sidebar</div>
         </Grid>
         <Grid item sm={9}>
+          <div>header</div>
           {children}
         </Grid>
       </Grid>
