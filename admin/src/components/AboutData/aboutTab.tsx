9ba23f02d146170c11099483a20e63d4d18de0b0
Tofik Abdullayev <10906783+tofikabdullayev@users.noreply.github.com>
2020-05-07 23:11:33 +0200

    about ui done

--- admin/src/components/AboutData/aboutTab.tsx
+++ admin/src/components/AboutData/aboutTab.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import { About } from '../../store/interfaces';
@@ -24,11 +23,7 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`about-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box p={3}>{children}</Box>}
     </div>
   );
 }
@@ -45,7 +40,22 @@ export interface AboutTabProps {
 }
 
 const AboutTab: React.FC<AboutTabProps> = ({ data }) => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState<number>(0);
+  const [azTitle, setAzTitle] = useState<string>('');
+  const [azDescription, setAzDescription] = useState<string>('');
+  const [enTitle, setEnTitle] = useState<string>('');
+  const [enDescription, setEnDescription] = useState<string>('');
+  const [ruTitle, setRuTitle] = useState<string>('');
+  const [ruDescription, setRuDescription] = useState<string>('');
+
+  useEffect(() => {
+    setAzTitle(data.title.az);
+    setAzDescription(data.description.az);
+    setEnTitle(data.title.en);
+    setEnDescription(data.description.en);
+    setRuTitle(data.title.ru);
+    setRuDescription(data.description.ru);
+  }, [data]);
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
@@ -57,6 +67,8 @@ const AboutTab: React.FC<AboutTabProps> = ({ data }) => {
           value={value}
           onChange={handleChange}
           aria-label="about tabs example"
+          style={{ background: 'rgb(0%,52.549744%,63.137817%)' }}
+          TabIndicatorProps={{ style: { background: '#fff' } }}
         >
           <Tab label="Az" {...a11yProps(0)} />
           <Tab label="En" {...a11yProps(1)} />
@@ -66,18 +78,18 @@ const AboutTab: React.FC<AboutTabProps> = ({ data }) => {
       <TabPanel value={value} index={0}>
         <TextField
           label="About block title [AZ]"
-          value={data.title.az}
+          value={azTitle}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setAzTitle(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
           //   className={classes.fullTextFields}
         />
         <TextField
           label="About block description [AZ]"
-          value={data.description.az}
+          value={azDescription}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setAzDescription(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
           multiline
@@ -86,19 +98,19 @@ const AboutTab: React.FC<AboutTabProps> = ({ data }) => {
       </TabPanel>
       <TabPanel value={value} index={1}>
         <TextField
-          label="About block title [AZ]"
-          value={data.title.en}
+          label="About block title [EN]"
+          value={enTitle}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setEnTitle(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
           //   className={classes.fullTextFields}
         />
         <TextField
-          label="About block title [AZ]"
-          value={data.description.en}
+          label="About block description [EN]"
+          value={enDescription}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setEnDescription(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
           multiline
@@ -107,23 +119,23 @@ const AboutTab: React.FC<AboutTabProps> = ({ data }) => {
       </TabPanel>
       <TabPanel value={value} index={2}>
         <TextField
-          label="About block title [AZ]"
-          value={data.title.ru}
+          label="About block title [RU]"
+          value={ruTitle}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setRuTitle(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
           //   className={classes.fullTextFields}
         />
         <TextField
-          label="About block title [AZ]"
-          value={data.description.ru}
+          label="About block description [RU]"
+          value={ruDescription}
           fullWidth
-          onChange={(e) => console.log(e)}
+          onChange={(e) => setRuDescription(e.target.value)}
           //   error={!adressAZ.isValid && adressAZ.touched}
           required
-          //   className={classes.fullTextFields}
           multiline
+          //   className={classes.fullTextFields}
         />
       </TabPanel>
     </div>
