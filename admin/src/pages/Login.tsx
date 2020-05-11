60ea6fa9af79d99311cad7218b714ae277b2894f
Tofik Abdullayev <10906783+tofikabdullayev@users.noreply.github.com>
2020-05-11 23:06:53 +0200

    login page ui logic done

--- admin/src/pages/Login.tsx
+++ admin/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -6,11 +6,28 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import logo from '../logo.png';
+import { initialitemTitle, ItemTitle } from './Products';
 
 export interface LoginPageProps {}
 
 const LoginPage: React.FC<LoginPageProps> = () => {
+  const [username, setUserName] = useState<ItemTitle>(initialitemTitle);
+  const [password, setPassword] = useState<ItemTitle>(initialitemTitle);
   const classes = useStyles();
+
+  const titleChangehandler = (
+    value: string,
+    setter: (newData: ItemTitle) => void
+  ) => {
+    const currentState: ItemTitle = {
+      value: value,
+      touched: true,
+      isValid: value.trim().length > 5,
+    };
+
+    setter(currentState);
+  };
+
   return (
     <div className={classes.loginPage}>
       <Card variant="outlined">
@@ -23,22 +40,39 @@ const LoginPage: React.FC<LoginPageProps> = () => {
           >
             Enter your username and password
           </Typography>
-          <form className={classes.loginForm}>
+          <form
+            className={classes.loginForm}
+            onSubmit={(e) => {
+              e.preventDefault();
+              window.location.href = '/';
+            }}
+          >
             <TextField
               name="username"
               type="text"
               label="Username"
+              value={username.value}
               fullWidth
               className={classes.loginInput}
+              onChange={(e) => titleChangehandler(e.target.value, setUserName)}
+              error={!username.isValid && username.touched}
             />
             <TextField
               name="password"
               type="password"
               label="Password"
+              value={password.value}
               fullWidth
               className={classes.loginInput}
+              onChange={(e) => titleChangehandler(e.target.value, setPassword)}
+              error={!password.isValid && password.touched}
             />
-            <Button variant="contained" className={classes.loginBtn}>
+            <Button
+              variant="contained"
+              className={classes.loginBtn}
+              disabled={!(username.isValid && password.isValid)}
+              type="submit"
+            >
               Login
             </Button>
           </form>
