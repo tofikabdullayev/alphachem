c27fc6e57979056251ff78453fe6af45866f598f
Tofig Abdullayev <10906783+tofikabdullayev@users.noreply.github.com>
2020-05-17 17:41:30 +0200

    login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/auth');
 const router = Router();
@@ -13,7 +13,6 @@ router.post(
     check('password', 'Password should be min 6 letters').isLength({ min: 6 }),
   ],
   async (req, res) => {
-    console.log(process.env.JWTSECRET);
     try {
       const errors = validationResult(req);
 
@@ -44,51 +43,48 @@ router.post(
   }
 );
 
-// // /api/auth/login
-// router.post(
-//   '/login',
-//   [
-//     check('email', 'Введите корректный email').normalizeEmail().isEmail(),
-//     check('password', 'Введите пароль').exists(),
-//   ],
-//   async (req, res) => {
-//     try {
-//       const errors = validationResult(req);
+// /api/auth/login
+router.post(
+  '/login',
+  [
+    check('username', 'Invalid login data').exists(),
+    check('password', 'Invalid login data').exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-//       if (!errors.isEmpty()) {
-//         return res.status(400).json({
-//           errors: errors.array(),
-//           message: 'Некорректный данные при входе в систему',
-//         });
-//       }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Invalid login data',
+        });
+      }
 
-//       const { email, password } = req.body;
+      const { username, password } = req.body;
 
-//       const user = await User.findOne({ email });
+      const user = await User.findOne({ username });
 
-//       if (!user) {
-//         return res.status(400).json({ message: 'Пользователь не найден' });
-//       }
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid login data' });
+      }
 
-//       const isMatch = await bcrypt.compare(password, user.password);
+      const isMatch = await bcrypt.compare(password, user.password);
 
-//       if (!isMatch) {
-//         return res
-//           .status(400)
-//           .json({ message: 'Неверный пароль, попробуйте снова' });
-//       }
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid login data' });
+      }
 
-//       const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), {
-//         expiresIn: '1h',
-//       });
+      const token = jwt.sign({ userId: user.id }, process.env.JWTSECRET, {
+        expiresIn: '1d',
+      });
 
-//       res.json({ token, userId: user.id });
-//     } catch (e) {
-//       res
-//         .status(500)
-//         .json({ message: 'Что-то пошло не так, попробуйте снова' });
-//     }
-//   }
-// );
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
+);
 
 module.exports = router;
